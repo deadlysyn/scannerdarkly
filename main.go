823d6e852c1b0f2fd62bacab6940919e0c83db7a
Mike Hoskins <mrh@devopsdreams.io>
2020-11-04 18:02:27 -0500

    list hosted zones

--- main.go
+++ main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"fmt"
+	"log"
 
-	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/route53"
 )
@@ -12,22 +12,29 @@ func main() {
 	sess := session.Must(session.NewSession())
 	svc := route53.New(sess)
 
-	input := &route53.GetHostedZoneInput{}
-	result, err := svc.GetHostedZone(input)
+	input := &route53.ListHostedZonesInput{}
+	result, err := svc.ListHostedZones(input)
 	if err != nil {
-		if aerr, ok := err.(awserr.Error); ok {
-			switch aerr.Code() {
-			case route53.ErrCodeNoSuchHostedZone:
-				fmt.Println(route53.ErrCodeNoSuchHostedZone, aerr.Error())
-			case route53.ErrCodeInvalidInput:
-				fmt.Println(route53.ErrCodeInvalidInput, aerr.Error())
-			default:
-				fmt.Println(aerr.Error())
-			}
-		} else {
-			fmt.Println(err.Error())
-		}
-		return
+		log.Fatal(err)
 	}
-	fmt.Printf("%+v", result)
+	fmt.Println(result)
+
+	// input := &route53.GetHostedZoneInput{}
+	// result, err := svc.GetHostedZone(input)
+	// if err != nil {
+	// 	if aerr, ok := err.(awserr.Error); ok {
+	// 		switch aerr.Code() {
+	// 		case route53.ErrCodeNoSuchHostedZone:
+	// 			fmt.Println(route53.ErrCodeNoSuchHostedZone, aerr.Error())
+	// 		case route53.ErrCodeInvalidInput:
+	// 			fmt.Println(route53.ErrCodeInvalidInput, aerr.Error())
+	// 		default:
+	// 			fmt.Println(aerr.Error())
+	// 		}
+	// 	} else {
+	// 		fmt.Println(err.Error())
+	// 	}
+	// 	return
+	// }
+	// fmt.Printf("%+v", result)
 }
