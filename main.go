5370fd795963a35e193786a838f64b90ed917d97
Mike Hoskins <mrh@devopsdreams.io>
2020-11-15 17:31:13 -0500

    support config via environment

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"log"
+	"os"
+	"strings"
 
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/route53"
@@ -16,20 +18,56 @@ type dnsRecord struct {
 // DB maps Route53 Zone IDs to slices of dnsRecords
 var DB = make(map[string][]dnsRecord)
 
+// PORTS ports to check on DNS targets (default: 80 8080 443 8443)
+var PORTS []string
+
+// ZONES Route53 zone IDs to audit (default: all in account)
+var ZONES []string
+
+func init() {
+	ports := strings.TrimSpace(os.Getenv("PORTS"))
+	if len(ports) > 0 {
+		for _, p := range strings.Split(ports, " ") {
+			PORTS = append(PORTS, p)
+		}
+	} else {
+		PORTS = []string{
+			"80",
+			"8080",
+			"443",
+			"8443",
+		}
+	}
+
+	zones := strings.TrimSpace(os.Getenv("ZONES"))
+	if len(zones) > 0 {
+		for _, z := range strings.Split(zones, " ") {
+			ZONES = append(ZONES, z)
+		}
+	}
+}
+
 func main() {
 	s := session.Must(session.NewSession())
 	r := route53.New(s)
 
-	ids, err := getPublicZoneIds(r)
-	if err != nil {
-		log.Fatal(err)
+	if len(ZONES) == 0 {
+		ids, err := getPublicZoneIds(r)
+		if err != nil {
+			log.Fatal(err)
+		}
+		populateDB(r, ids)
+	} else {
+		populateDB(r, ZONES)
 	}
 
+	// scan()
+	reportCSV()
+	// reportJSON()
+}
+
+func populateDB(r *route53.Route53, ids []string) {
 	for _, v := range ids {
-		// populates DB
 		getResourceRecords(r, v)
 	}
-
-	// fmt.Printf("%+v", DB)
-	report()
 }
