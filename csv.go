cde57deaf6933623c0510bc1e47453f76a300709
Mike Hoskins <mrh@devopsdreams.io>
2020-11-14 23:32:51 -0500

    add header; remove unnecessary variable

--- csv.go
+++ csv.go
@@ -10,23 +10,30 @@ func report() {
 	w := csv.NewWriter(os.Stdout)
 	defer w.Flush()
 
+	w.Write([]string{
+		"Name",
+		"Zone ID",
+		"Type",
+		"Values",
+	})
+
 	for k, v := range DB {
 		switch v.Type {
 		case "A", "AAAA", "CNAME":
-			line := []string{
+			w.Write([]string{
 				k,
+				v.ID,
 				v.Type,
 				v.Values[0],
-			}
-			w.Write(line)
+			})
 			if len(v.Values) > 1 {
 				for i := 1; i < len(v.Values); i++ {
-					line := []string{
+					w.Write([]string{
+						"",
 						"",
 						"",
 						v.Values[i],
-					}
-					w.Write(line)
+					})
 				}
 			}
 		default:
