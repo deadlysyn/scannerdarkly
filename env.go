869e6c2c05b73282818bffb949daa214cb5091f3
Mike Hoskins <mrh@devopsdreams.io>
2020-11-17 16:00:27 -0500

    factor out env parsing

---
+++ env.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+// PORTS ports to check on DNS targets (default: 80 8080 443 8443)
+var PORTS []string
+
+// TIMEOUT net.Dial timeout for network tests (default 10 seconds)
+var TIMEOUT time.Duration
+
+// ZONES Route53 zone IDs to audit (default: all in account)
+var ZONES []string
+
+func parseEnv() {
+	ports := strings.TrimSpace(os.Getenv("PORTS"))
+	if len(ports) > 0 {
+		for _, p := range strings.Split(ports, " ") {
+			PORTS = append(PORTS, p)
+		}
+	} else {
+		PORTS = []string{
+			"80",
+			"8080",
+			"443",
+			"8443",
+		}
+	}
+
+	timeout := strings.TrimSpace(os.Getenv(("TIMEOUT")))
+	if len(timeout) > 0 {
+		t, _ := strconv.Atoi(timeout)
+		TIMEOUT = time.Duration(t) * time.Second
+	} else {
+		TIMEOUT = 10 * time.Second
+	}
+
+	zones := strings.TrimSpace(os.Getenv("ZONES"))
+	if len(zones) > 0 {
+		for _, z := range strings.Split(zones, " ") {
+			ZONES = append(ZONES, z)
+		}
+	}
+}
--- main.go
+++ main.go
@@ -2,10 +2,6 @@ package main
 
 import (
 	"log"
-	"os"
-	"strconv"
-	"strings"
-	"time"
 
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/route53"
@@ -22,44 +18,8 @@ type dnsRecord struct {
 // DB maps Route53 Zone IDs to slices of dnsRecords
 var DB = make(map[string][]dnsRecord)
 
-// PORTS ports to check on DNS targets (default: 80 8080 443 8443)
-var PORTS []string
-
-// TIMEOUT net.Dial timeout for network tests (default 10 seconds)
-var TIMEOUT time.Duration
-
-// ZONES Route53 zone IDs to audit (default: all in account)
-var ZONES []string
-
 func init() {
-	ports := strings.TrimSpace(os.Getenv("PORTS"))
-	if len(ports) > 0 {
-		for _, p := range strings.Split(ports, " ") {
-			PORTS = append(PORTS, p)
-		}
-	} else {
-		PORTS = []string{
-			"80",
-			"8080",
-			"443",
-			"8443",
-		}
-	}
-
-	timeout := strings.TrimSpace(os.Getenv(("TIMEOUT")))
-	if len(timeout) > 0 {
-		t, _ := strconv.Atoi(timeout)
-		TIMEOUT = time.Duration(t) * time.Second
-	} else {
-		TIMEOUT = 10 * time.Second
-	}
-
-	zones := strings.TrimSpace(os.Getenv("ZONES"))
-	if len(zones) > 0 {
-		for _, z := range strings.Split(zones, " ") {
-			ZONES = append(ZONES, z)
-		}
-	}
+	parseEnv()
 }
 
 func main() {
