de2d0e6ca93b413272d141063acfbddd6d7b5f31
Mike Hoskins <mrh@devopsdreams.io>
2021-07-13 23:41:03 -0400

    bootstrap base command and config parsing

--- cmd/root.go
+++ cmd/root.go
@@ -4,8 +4,6 @@ import (
 	"fmt"
 	"log"
 
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/route53"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
@@ -25,11 +23,9 @@ var (
 	DB = make(map[string][]dnsRecord)
 
 	RootCmd = &cobra.Command{
-		Use:   "gac",
-		Short: "Find dark (stale) Route53 records",
-		// Uncomment the following line if your bare application
-		// has an action associated with it:
-		//      Run: func(cmd *cobra.Command, args []string) { },
+		Use:   "d",
+		Short: "Scan for dark (stale) Route53 records",
+		Run:   scanner,
 	}
 )
 
@@ -42,52 +38,54 @@ func Execute() {
 func init() {
 	cobra.OnInitialize(initConfig)
 
-	RootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.google-admin.yaml)")
+	RootCmd.PersistentFlags().StringVarP(&cfgFile, "config", "c", "config.yml", "config file")
 }
 
-// initConfig reads in config file and ENV variables if set.
 func initConfig() {
-	if cfgFile != "" { // enable ability to specify config file via flag
+	viper.SetConfigType("yaml")
+
+	if cfgFile != "" {
 		viper.SetConfigFile(cfgFile)
 	}
 
-	viper.SetConfigName(".google-admin") // name of config file (without extension)
-	viper.AddConfigPath("$HOME")         // adding home directory as first search path
-	viper.SetEnvPrefix("google_admin")
-	viper.AutomaticEnv() // read in environment variables that match
+	viper.SetEnvPrefix("pkd")
+	viper.AutomaticEnv()
 
-	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	c := viper.Get("config")
+	if c != nil {
+		viper.SetConfigFile(c.(string))
 	}
 
+	if err := viper.ReadInConfig(); err != nil {
+		log.Fatalf("ERROR: %v", err)
+	}
 }
 
-// func init() {
-// 	parseEnv()
+func scanner(cmd *cobra.Command, args []string) {
+	fmt.Println("hello from scanner")
+}
+
+// func initSession() {
+// 	s := session.Must(session.NewSession())
+// 	r := route53.New(s)
+
+// 	if len(ZONES) == 0 {
+// 		ids, err := getPublicZoneIds(r)
+// 		if err != nil {
+// 			log.Fatal(err)
+// 		}
+// 		populateDB(r, ids)
+// 	} else {
+// 		populateDB(r, ZONES)
+// 	}
+
+// 	scan()
+// 	reportCSV()
+// 	// reportJSON()
 // }
 
-func initSession() {
-	s := session.Must(session.NewSession())
-	r := route53.New(s)
-
-	if len(ZONES) == 0 {
-		ids, err := getPublicZoneIds(r)
-		if err != nil {
-			log.Fatal(err)
-		}
-		populateDB(r, ids)
-	} else {
-		populateDB(r, ZONES)
-	}
-
-	scan()
-	reportCSV()
-	// reportJSON()
-}
-
-func populateDB(r *route53.Route53, ids []string) {
-	for _, v := range ids {
-		getResourceRecords(r, v)
-	}
-}
+// func populateDB(r *route53.Route53, ids []string) {
+// 	for _, v := range ids {
+// 		getResourceRecords(r, v)
+// 	}
+// }
